8af9eb3b52fee42598446bd1b19adca4516b1a4c
Quang Nguyen <quangnguyen116hb@gmail.com>
2017-02-13 09:55:36 +0700

    continue...

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 	function helloWorld(){
 		console.log('done');
 		$("#typed").typed({
-			strings: ["Hi!", "I'm a student.", "I'm a blogger.", "I'm a developer.", "I'm Viet Anh.", "Nice to meet you!"],
+			strings: ["Hi!", "I'm a student.", "I'm a blogger.", "I'm a developer.", "I'm Quang Nguyen.", "Nice to meet you!"],
 			cursorChar: " |",
 			typeSpeed: 45,
 			callback: function(){
